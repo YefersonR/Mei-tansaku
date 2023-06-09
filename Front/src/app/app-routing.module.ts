474140e13af20a57a60f11e4a6969b84c2930449
YefersonR <Yefersonrubio27@gmail.com>
2023-06-08 21:26:44 -0400

    setting routes and creating services

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'login'
+  },
+  {
+    path:'signup'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
