716d48b01b9c9352f7f74aae742f2761c7389d07
elvinpoueriet <108239541+elvinpoueriet@users.noreply.github.com>
2023-06-13 14:49:52 -0400

    create login and registrer

---
+++ Front/src/app/components/loginempresa/loginempresa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginempresa',
+  templateUrl: './loginempresa.component.html',
+  styleUrls: ['./loginempresa.component.css']
+})
+export class LoginempresaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
