9b5ef2e4104e93b6a5fa0b977e8da5df58fd5a03
Feenri03 <rijoferwis@gmail.com>
2023-08-10 18:53:37 -0400

    category2

--- Front/src/app/components/category/category.component.ts
+++ Front/src/app/components/category/category.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
+
 export class CategoryComponent implements OnInit {
+  categories: any[] = [];
 
+  constructor(private categoryService: CategoryService) {}
 
-  constructor() { }
-  
-  ngOnInit(){
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data.slice(0, 3);
+    });
   }
-
-
 }
