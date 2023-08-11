3eb328a15dd660d9e1140dee577bb9ed1a2ab73a
elvinpoueriet <108239541+elvinpoueriet@users.noreply.github.com>
2023-08-11 18:27:48 -0400

    Ir al buscador
    boton en el navbar par air al buscador y se borro la barra de navegacion

--- Front/src/app/components/navbar/navbar.component.ts
+++ Front/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { CartService } from 'src/app/services/cart.service';
 import { productCart } from 'src/app/interfaces/productCart.interface';
-
 
 @Component({
   selector: 'app-navbar',
@@ -21,6 +18,26 @@ export class NavbarComponent implements OnInit {
   isSidebarOpen = false;
   cart: productCart[];
 
+  constructor(
+    private categoryService: CategoryService,
+    private cartService: CartService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchCategories();
+    this.cart = this.cartService.getCarrito();
+  }
+
+  fetchCategories() {
+    this.categoryService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
   openDropdown() {
     this.isOpen = !this.isOpen;
@@ -30,30 +47,11 @@ export class NavbarComponent implements OnInit {
     this.isOpening = !this.isOpening;
   }
 
-
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-
-  constructor(private categoryService: CategoryService,private cartService:CartService, private router: Router) { }
-
-
-  ngOnInit(): void {
-    this.fetchCategories();
-    this.cart= this.cartService.getCarrito()
+  irABuscador() {
+    this.router.navigate(['/buscador']); // Cambia '/buscador' por la ruta real de tu componente "buscador"
   }
-
-
-  fetchCategories() {
-    this.categoryService.getCategories().subscribe(categories => {
-      this.categories = categories;
-    })
-  }
-
-  toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
-  }
-
-
 }
